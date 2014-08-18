11221b37354e3c689f9f2562e2514e1015252ff9
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2014-08-17 21:06:29 -0500

    Test exec module directly

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 var nixt = require('nixt');
 var fs = require('fs');
 
@@ -18,3 +19,15 @@ beforeEach(function() {
   }).cwd('examples/yell')
     .path('examples/yell/bin');
 });
+
+/**
+ * Asserts that `stream` emits the data `expected` and then
+ * calls the `done` callback.
+ */
+assert.data = function(stream, expected, done) {
+  stream.on('error', done);
+  stream.on('data', function(data) {
+    assert.equal(String(data), expected);
+    done();
+  });
+};

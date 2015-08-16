451ece7787d8d54a143a86e02a8a5c6c73bc24d6
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2015-08-16 12:11:17 -0700

    Spec out setting the exit code

--- test/server.js
+++ test/server.js
@@ -15,4 +15,29 @@ describe('server', function() {
     });
     server.run([]);
   });
+  it('returns a readable stream', function(done) {
+    var server = hoagie.createServer(function(req, res) {
+      res.end('OK');
+    });
+
+    var stream = server.run([]);
+
+    assert.equal(stream.readable, true);
+
+    stream.on('data', function(str) {
+      assert.equal(str, 'OK');
+      done();
+    });
+  });
+  it('sets the exit code', function(done) {
+    hoagie.createServer(function(req, res) {
+      res.exitCode = 1;
+      res.end();
+    }).run([]).on('readable', function() {
+      assert.equal(process.exitCode, 0);
+    }).on('end', function() {
+      assert.equal(process.exitCode, 1);
+      done();
+    });
+  });
 });

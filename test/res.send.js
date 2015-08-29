97d6b035e3553ea2837200aa30170e7a7330cc83
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2015-08-29 09:58:04 -0700

    Move output helper into support directory

--- test/res.send.js
+++ test/res.send.js
@@ -1,20 +1,20 @@
 var hoagie = require('..');
 var assert = require('assert');
-var Output = require('./output');
+var stdio = require('./support');
 
 /* jshint mocha:true */
 
 describe('res.send', function() {
   it('writes to process.stdout', function(done) {
+    var stdout = new stdio.Output();
     var app = hoagie();
-    var out = new Output();
 
     app.use(function(req, res) {
       res.send('OK');
     });
 
-    app.run([], out).on('finish', function() {
-      assert.equal(out.data, 'OK\n');
+    app.run([], stdout).on('finish', function() {
+      assert.equal(stdout.data, 'OK\n');
       done();
     });
   });

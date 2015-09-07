766c0ff9c6396ffac634d9c8a6a21c6339346f89
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2015-09-07 22:43:07 +0200

    Support multiple middleware in app.use

--- test/router.js
+++ test/router.js
@@ -5,28 +5,37 @@ var assert = require('assert');
 
 describe('router', function() {
   it('runs middleware that match the command', function(done) {
-    hoagie().use('foo', function() {
+    var app = hoagie();
+
+    app.use('foo', function() {
       done(new Error('Middleware should not have been called'));
-    }).use('bar', function() {
+    });
+
+    app.use('bar', function() {
       done();
-    }).run([
+    });
+
+    app.run([
       'bar'
     ]);
   });
   it('runs all middleware that match the command', function(done) {
+    var app = hoagie();
     var arr = [];
 
-    hoagie().use('foo', function(/* req, res, next */) {
+    app.use('foo', function(/* req, res, next */) {
       done(new Error('Middleware should not have been called'));
-    }).use('bar', function(req, res, next) {
+    });
+
+    app.use('bar', function(req, res, next) {
       arr.push('bar 1');
       next();
-    }).use('bar', function(req, res, next) {
+    }, function(req, res, next) {
       arr.push('bar 2');
       next();
-    }).run([
-      'bar'
-    ]).on('finish', function() {
+    });
+
+    app.run([ 'bar' ]).on('finish', function() {
       assert.deepEqual(arr, ['bar 1', 'bar 2']);
       done();
     });

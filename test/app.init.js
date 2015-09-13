6e8a766b8f9b1d902abefa084d542762714c5bd6
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2015-09-13 14:31:44 -0700

    Remove the extname from the program name

--- test/app.init.js
+++ test/app.init.js
@@ -4,15 +4,21 @@ var assert = require('assert');
 /* jshint mocha:true */
 
 describe('app.init', function() {
+  var argv = process.argv.slice();
   var app;
 
   beforeEach(function() {
-    app = hoagie().init();
+    app = hoagie();
+  });
+  afterEach(function() {
+    process.argv = argv;
   });
   it('creates a new router', function() {
-    assert.ok(app._router);
+    assert.ok(app.init()._router);
   });
   it('sets the program basename', function() {
-    assert.equal(app.get('program'), '_mocha');
+    process.argv[1] = __filename;
+
+    assert.equal(app.init().get('program'), 'app.init');
   });
 });

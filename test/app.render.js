ca122acb7e5af9031069b3f650a7521be0c716c0
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2015-10-18 17:07:46 -0700

    Add tests for application and request locals

--- test/app.render.js
+++ test/app.render.js
@@ -11,17 +11,34 @@ describe('app.render', function() {
       'Hello from hoagie!\n');
   });
   it('throws an error if the view cannot be loaded', function() {
-    var app = hoagie().set('program', 'hoagie');
+    assert.throws(example, /ENOENT, no such file or directory/);
 
-    assert.throws(function() {
-      app.render(__dirname + '/fixtures/missing.txt');
-    }, /ENOENT, no such file or directory/);
+    function example() {
+      hoagie().render(__dirname + '/fixtures/missing.txt');
+    }
   });
   it('throws an error if there is a problem rendering the view', function() {
-    var app = hoagie().set('program', 'hoagie');
+    assert.throws(example, /foo is not defined/);
 
-    assert.throws(function() {
-      app.render(__dirname + '/fixtures/error.txt');
-    }, /foo is not defined/);
+    function example() {
+      hoagie().render(__dirname + '/fixtures/error.txt');
+    }
+  });
+  it('uses provided locals', function() {
+    var app = hoagie();
+
+    assert.equal(app.render(__dirname + '/fixtures/locals.txt', {
+      one: 'wow',
+      two: 'amaze'
+    }), 'wow amaze\n');
+  });
+  it('uses application locals', function() {
+    var app = hoagie();
+
+    app.locals.one = 'wow';
+
+    assert.equal(app.render(__dirname + '/fixtures/locals.txt', {
+      two: 'amaze'
+    }), 'wow amaze\n');
   });
 });

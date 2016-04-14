e9f1ea6425ef586794a6430a9910aee5bfe00c67
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2016-04-14 14:10:46 -0700

    Exit nonzero if there is an error in the stack

--- test/app.handle.js
+++ test/app.handle.js
@@ -13,6 +13,24 @@ describe('app.handle', function() {
 
     invoke(app).run([]).expect(0, done);
   });
-  it('exits nonzero if there is an error');
-  it('exits with the error code if present');
+  it('exits nonzero if there is an error', function (done) {
+    var app = hoagie();
+
+    app.use(function(req, res, next) { // jshint ignore:line
+      next(new Error('boom'));
+    });
+
+    invoke(app).run([]).expect(8, done);
+  });
+  it('exits with the error code if present', function (done) {
+    var app = hoagie();
+
+    app.use(function(req, res, next) { // jshint ignore:line
+      var err = new Error('boom');
+      err.code = 128;
+      next(err);
+    });
+
+    invoke(app).run([]).expect(128, done);
+  });
 });
